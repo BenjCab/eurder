c52b3d14943506cedb449ed9d0b4a3cdbd0d4951
ben <benjamin.cabaux@gmail.com>
2021-03-31 14:12:52 +0200

    fixed order

--- src/main/java/com/switchfully/eurder/api/Dtos/Order/CreateItemGroupDTO.java
+++ src/main/java/com/switchfully/eurder/api/Dtos/Order/CreateItemGroupDTO.java
@@ -5,13 +5,13 @@ import com.switchfully.eurder.domain.Item;
 import java.util.UUID;
 
 public class CreateItemGroupDTO {
-    private final UUID itemID;
-    private final int amount;
-
+    private UUID itemID;
+    private int amount;
+/*
     public CreateItemGroupDTO(UUID itemID, int amount) {
         this.itemID = itemID;
         this.amount = amount;
-    }
+    }*/
 
     public UUID getItemID() {
         return itemID;
